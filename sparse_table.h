a94a73585163d2bff7611b59a96acfbc8510a014
ducbrick <95757159+ducbrick@users.noreply.github.com>
2022-08-29 21:44:04 +0700

    Update sparse_table.h

--- sparse_table.h
+++ sparse_table.h
@@ -1,21 +1,21 @@
 struct Sparse_Table {
-  typedef long long Type;
+  typedef int Type;
 
   inline Type f(Type a, Type b) {
-    return min(a, b);
+    return max(a, b);
   }
 
   vector <vector <Type>> st;
 
   void build(Type *a, int n) {
-    int k = log2(n);
+    int k = __lg(n);
 
     st.resize(n + 1, vector <Type> (k + 1));
 
     for (int i = 0; i <= n; i++)
       st[i][0] = a[i];
 
-    for (int k = 1; k <= log2(n); k++)
+    for (int k = 1; k <= __lg(n); k++)
       for (int i = 0; i + (1 << k) - 1 <= n; i++) {
         int j = i + (1 << (k - 1));
         st[i][k] = f(st[i][k - 1], st[j][k - 1]);
@@ -23,7 +23,7 @@ struct Sparse_Table {
   }
 
   Type query(int l, int r) {
-    int k = log2(r - l + 1);
+    int k = __lg(r - l + 1);
     r = r - (1 << k) + 1;
     return f(st[l][k], st[r][k]);
   }
